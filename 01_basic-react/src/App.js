70ae2779f8e849f4bf29c1ecefc784b7b572ef06
Tabin Tariq <tabintariq9@gmail.com>
2024-02-06 23:25:07 +0530

    react project using create-react-app

---
+++ 01_basic-react/src/App.js
@@ -0,0 +1,12 @@
+import Test from "./Test"
+
+function App() {
+  return (
+    <>
+      <Test />
+      <h1>React-101 | Tabin Tariq</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01_basic-react/src/Test.js
@@ -0,0 +1,7 @@
+function Test () {
+    return (
+        <h2>Testing create component in Create-React-App</h2>
+    );
+}
+
+export default Test;
